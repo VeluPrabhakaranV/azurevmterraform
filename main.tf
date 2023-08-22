5bc9026737b708b29cc7e4c1116f7ea04e5d64fd
VeluPrabhakaranV <108867171+VeluPrabhakaranV@users.noreply.github.com>
2023-08-22 14:59:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,12 +21,12 @@ resource "azurerm_subnet" "example" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "example-pip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method  = "Dynamic"
-}
+#resource "azurerm_public_ip" "example" {
+  #name                = "example-pip"
+  #location            = azurerm_resource_group.example.location
+  #resource_group_name = azurerm_resource_group.example.name
+  #allocation_method  = "Dynamic"
+#}
 
 resource "azurerm_network_interface" "example" {
   name                = "example-nic"
@@ -37,7 +37,7 @@ resource "azurerm_network_interface" "example" {
     name                          = "testconfiguration1"
     subnet_id                     = azurerm_subnet.example.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
+    #public_ip_address_id          = azurerm_public_ip.example.id
   }
 }
 
